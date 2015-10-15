85460189f8f207b2746f8ef71dcf07aade7f73b8
theshka <t.heshka@gmail.com>
2015-10-15 17:55:20 -0600

    update laout, add comment count

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,14 @@ $('a').each(function() {
        });
    }
 });
+
+/* * * CONFIGURATION VARIABLES * * */
+var disqus_shortname = 'theshka';
+
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function () {
+    var s = document.createElement('script'); s.async = true;
+    s.type = 'text/javascript';
+    s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
